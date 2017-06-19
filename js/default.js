6109e3a3bc2d46d90d86c88f76bb634ae7e71eee
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-06-19 17:54:23 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,10 +2,11 @@
 var number = Math.floor(Math.random()*100+1);
 var allNumber = 100;
 var partNumber = allNumber / 2;
-var point = 10;
+var message;
+var goal = 0;
 
 //ゲーム
-for(var i = 0; point <= 0; i　>　0){
+for(var point = 10;point - 1; point <= 0 || goal === 0){
   do{
     var answer = parseInt(window.prompt('残りポイント${allNumber}\n\n数あてゲーム！1～100から探してね！\nヒントが欲しい人は0を入力してね！'));
     if(answer < 0 || ansswer > 100){
@@ -15,7 +16,19 @@ for(var i = 0; point <= 0; i　>　0){
   
   if(answer === 0){
     partNumber = allNumber / 2;
-    if(answer <= partNumber
+    if(answer <= partNumber){
+      
+    }
+    
+    point -= 1;
   }else{
+    if(answer === number){
+      message = 'あたりでーーーーーーーす！';
+      goal = 1;
+    }else{
+      message = '残念がんば！';
+    }
+    
+    document.getEle,emmtById('choice').textContect = message;
   }
 }
