81bf4d3c195db43ba6e6c16dd57b5d2ae5cbdc5f
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-06-19 17:42:41 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,13 +1,13 @@
 //数字あてゲームの数字決め
-ver number = Math.floor(Math.random()*100);
-ver allNumber = 100;
-ver partNumber = allNumber / 2;
-ver point = 10;
+var number = Math.floor(Math.random()*100+1);
+var allNumber = 100;
+var partNumber = allNumber / 2;
+var point = 10;
 
 //ゲーム
-for(ver i = 0; point <= 0; i>0;){
+for(var i = 0; point <= 0; i>0;){
   do{
-    ver answer = parseInt(window.prompt('残りポイント${allNumber}\n\n数あてゲーム！1～100から探してね！\nヒントが欲しい人は0を入力してね！'));
+    var answer = parseInt(window.prompt('残りポイント${allNumber}\n\n数あてゲーム！1～100から探してね！\nヒントが欲しい人は0を入力してね！'));
     if(answer < 0 || ansswer > 100){
       window.prompt('値が正しくないでござる');
     }
