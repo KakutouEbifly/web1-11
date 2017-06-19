e4fa3d3df059380bd4b9b9792c1feb4b75a3cbf0
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-06-19 17:55:17 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,7 +9,7 @@ var goal = 0;
 for(var point = 10;point - 1; point <= 0 || goal === 0){
   do{
     var answer = parseInt(window.prompt('残りポイント${allNumber}\n\n数あてゲーム！1～100から探してね！\nヒントが欲しい人は0を入力してね！'));
-    if(answer < 0 || ansswer > 100){
+    if(answer < 0 || answer > 100){
       window.prompt('値が正しくないでござる');
     }
   }while(answer < 0 || ansswer > 100);
