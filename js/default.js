257e6add72c2c82b9cb7e65ab2e82fd69262577a
KakutouEbifly <0ty3263000b474x@ezweb.ne.jp>
2017-06-19 17:38:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,11 @@
 //数字あてゲームの数字決め
-ver number = Manth.floor(Manth.random()*100+1);
+ver number = Math.floor(Math.random()*100+1);
 ver allNumber = 100;
 ver partNumber = allNumber / 2;
+ver point = 10;
 
 //ゲーム
-for(ver i = 0; ; i>0;){
+for(ver i = 0; point <= 0; i>0;){
   do{
     ver answer = parseInt(window.prompt('残りポイント${allNumber}\n\n数あてゲーム！1～100から探してね！\nヒントが欲しい人は0を入力してね！'));
     if(answer < 0 || ansswer > 100){
